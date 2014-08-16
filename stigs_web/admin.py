c5507d9e5a5dc7e01adc2ec44353005701555bcd
Christoph Brückmann <chrisnain@hotmail.com>
2014-08-16 21:49:24 +0200

    Added django User model as Stig model foreign key.

--- stigs_web/admin.py
+++ stigs_web/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from stigs_web.models import Stig
 
+
+class StigAdmin(admin.ModelAdmin):
+    fields = ['title', 'content', 'user', 'location', 'pub_date']
+
 # Register your models here.
-admin.site.register(Stig)
+admin.site.register(Stig, StigAdmin)
--- stigs_web/models.py
+++ stigs_web/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Stig(models.Model):
-    title = models.CharField(max_length=200)
+class UserData(models.Model):
+    user = models.ForeignKey(User)
+
+    class Meta:
+        abstract = True
+
+
+class Stig(UserData):
+    title = models.CharField(max_length=200, null=False)
     content = models.TextField()
     location = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
